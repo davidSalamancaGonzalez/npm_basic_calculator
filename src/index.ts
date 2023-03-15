3462e73275d96ecddb80ea941617895b2af1705f
David Salamanca Gonzalez <davidsg@in2.es>
2023-03-15 18:59:23 +0100

    Package done

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export class Calc {
+  sum(num1: number, num2: number) {
+    return num1 + num2;
+  }
+  substract(num1: number, num2: number) {
+    return num1 - num2;
+  }
+  multiply(num1: number, num2: number) {
+    return num1 * num2;
+  }
+  division(num1: number, num2: number) {
+    return num1 / num2;
+  }
+}
